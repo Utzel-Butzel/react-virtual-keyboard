256ac370710ca13c995b910a5bd00fdb35acd337
Lukas Winter <webmaster@geloescht.net>
2016-05-30 20:36:17 +0200

    Add select and clear methods, update dist

--- dist/Keyboard.js
+++ dist/Keyboard.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = require('react');
@@ -23,6 +25,8 @@ var _jqueryKeyboard = require('virtual-keyboard/dist/js/jquery.keyboard.js');
 var _jqueryKeyboard2 = _interopRequireDefault(_jqueryKeyboard);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -78,19 +82,36 @@ var _class = function (_React$Component) {
             (0, _jquery2.default)(_reactDom2.default.findDOMNode(this.refs.keyboard)).keyboard(this.props.options);
         }
     }, {
+        key: 'clear',
+        value: function clear() {
+            this.setState({ value: '' });
+        }
+    }, {
+        key: 'select',
+        value: function select() {
+            this.refs.keyboard.select();
+        }
+    }, {
         key: 'handleChange',
         value: function handleChange(event, input) {
             console.log("change", input, event, this);
             this.setState({ value: input });
-            this.props.callbackParent(input);
+            this.props.onChange(input);
         }
     }, {
         key: 'render',
         value: function render() {
+            var _props = this.props;
+            var options = _props.options;
+            var value = _props.value;
+            var onChange = _props.onChange;
+
+            var other = _objectWithoutProperties(_props, ['options', 'value', 'onChange']);
+
             if (this.props.options.type == 'textarea') {
-                var element = _react2.default.createElement('textarea', { ref: 'keyboard', name: this.props.name, className: this.props.options.class, value: this.state.value, onChange: this.handleChange });
+                var element = _react2.default.createElement('textarea', _extends({ ref: 'keyboard', value: this.state.value, onChange: this.handleChange }, other));
             } else {
-                var element = _react2.default.createElement('input', { ref: 'keyboard', name: this.props.name, className: this.props.options.class, value: this.state.value, onChange: this.handleChange });
+                var element = _react2.default.createElement('input', _extends({ ref: 'keyboard', value: this.state.value, onChange: this.handleChange }, other));
             }
             return _react2.default.createElement(
                 'div',
