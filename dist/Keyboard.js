72b5fcc3681d36ead229dfdbe64933e431a49028
Francisco Hodge <Francisco.Hodge@barco.com>
2017-10-20 12:44:13 -0400

    Update dist

--- dist/Keyboard.js
+++ dist/Keyboard.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
@@ -12,9 +12,9 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactDom = require('react-dom');
+var _propTypes = require('prop-types');
 
-var _reactDom2 = _interopRequireDefault(_reactDom);
+var _propTypes2 = _interopRequireDefault(_propTypes);
 
 var _jquery = require('jquery');
 
@@ -34,133 +34,159 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var VirtualKeyboard = function (_React$Component) {
-    _inherits(VirtualKeyboard, _React$Component);
+var VirtualKeyboard = function (_Component) {
+  _inherits(VirtualKeyboard, _Component);
 
-    function VirtualKeyboard(props) {
-        _classCallCheck(this, VirtualKeyboard);
+  function VirtualKeyboard(props) {
+    _classCallCheck(this, VirtualKeyboard);
 
-        var _this = _possibleConstructorReturn(this, (VirtualKeyboard.__proto__ || Object.getPrototypeOf(VirtualKeyboard)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (VirtualKeyboard.__proto__ || Object.getPrototypeOf(VirtualKeyboard)).call(this, props));
 
-        _this.state = { value: "", className: 'keyboard-wrapper' };
-        _this.handleChange = _this.handleChange.bind(_this);
-        return _this;
+    _this.handleChange = function (event, input) {
+      if (!input && event && event.target && typeof event.target.value != 'undefined') input = event.target.value;
+      if (_this.props.debug) {
+        console.log("Change", input);
+      }
+      _this.setState({
+        value: input
+      });
+      _this.props.onChange(input);
+    };
+
+    _this.state = {
+      value: "",
+      className: 'keyboard-wrapper'
+    };
+    return _this;
+  }
+
+  _createClass(VirtualKeyboard, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      // Set Value to Input Element on Accept
+      this.setState({
+        value: this.props.value
+      });
+
+      this.props.options.accepted = function (event, keyboard, el) {
+        _this2.handleChange('', el.value);
+        if (typeof _this2.props.onAccepted === 'function') {
+          _this2.props.onAccepted(el.value);
+        }
+        if (_this2.props.debug) {
+          console.log('The content "' + el.value + '" was accepted');
+        }
+      };
+
+      // Set Class to visible
+      this.props.options.visible = function () {
+        _this2.setState({
+          className: 'keyboard-wrapper open'
+        });
+      };
+
+      this.props.options.hidden = function () {
+        _this2.setState({
+          className: 'keyboard-wrapper'
+        });
+      };
+
+      // Set Value to Input Element on Change if prop set
+      if (this.props.options.updateOnChange === true) {
+        this.props.options.change = function (event, keyboard, el) {
+          _this2.handleChange('', keyboard.preview.value);
+          if (_this2.props.debug) {
+            console.log('The content "' + el.value + '" was changed');
+          }
+        };
+      }
+
+      // Add jQuery Keyboard to DOM Element
+      this.addKeyBoardToDOM();
+
+      // Update while typing if usePreview is false
+      if (this.props.options.usePreview === false) {
+        (0, _jquery2.default)(this.keyboardRef).on('keyboardChange', function (event, keyboard) {
+          _this2.handleChange(null, keyboard.preview.value);
+        });
+      }
     }
+  }, {
+    key: 'addKeyBoardToDOM',
+    value: function addKeyBoardToDOM() {
+      (0, _jquery2.default)(this.keyboardRef).keyboard(this.props.options);
+    }
+  }, {
+    key: 'clear',
+    value: function clear() {
+      this.setState({
+        value: ''
+      });
+    }
+  }, {
+    key: 'select',
+    value: function select() {
+      this.keyboardRef.select();
+    }
+  }, {
+    key: 'blur',
+    value: function blur() {
+      var keyboard = (0, _jquery2.default)(this.keyboardRef);
+      if (keyboard && typeof keyboard.getkeyboard === 'function' && typeof keyboard.getkeyboard().close === 'function') keyboard.getkeyboard().close();
+      this.keyboardRef.blur();
+    }
+  }, {
+    key: 'checkValidity',
+    value: function checkValidity() {
+      return this.keyboardRef.checkValidity();
+    }
+  }, {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() {
+      (0, _jquery2.default)(this.keyboardRef).remove();
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _this3 = this;
 
-    _createClass(VirtualKeyboard, [{
-        key: 'componentDidMount',
-        value: function componentDidMount() {
-            // Set Value to Input Element on Accept
-            this.setState({ value: this.props.value });
+      var _props = this.props,
+          options = _props.options,
+          value = _props.value,
+          validation = _props.validation,
+          onChange = _props.onChange,
+          other = _objectWithoutProperties(_props, ['options', 'value', 'validation', 'onChange']);
 
-            this.props.options.accepted = function (event, keyboard, el) {
-                this.handleChange('', el.value);
-                if (typeof this.props.onAccepted == 'function') {
-                    this.props.onAccepted(el.value);
-                }
-                if (this.props.debug) {
-                    console.log('The content "' + el.value + '" was accepted');
-                }
-            }.bind(this);
+      var element;
 
-            // Set Class to visible
-            this.props.options.visible = function function_name(event, keyboard, el) {
-                this.setState({ className: 'keyboard-wrapper open' });
-            }.bind(this);
+      if (this.props.options.type === 'textarea') element = _react2.default.createElement('textarea', _extends({ ref: function ref(node) {
+          return _this3.keyboardRef = node;
+        }, value: this.state.value, onChange: this.handleChange }, other));else element = _react2.default.createElement('input', _extends({ ref: function ref(node) {
+          return _this3.keyboardRef = node;
+        }, value: this.state.value, onChange: this.handleChange }, other));
 
-            this.props.options.hidden = function function_name(event, keyboard, el) {
-                this.setState({ className: 'keyboard-wrapper' });
-            }.bind(this);
+      return _react2.default.createElement(
+        'div',
+        { className: this.state.className },
+        ' ',
+        element,
+        ' '
+      );
+    }
+  }]);
 
-            // Set Value to Input Element on Change if prop set
-            if (this.props.options.updateOnChange == true) {
-                this.props.options.change = function (event, keyboard, el) {
-                    this.handleChange('', keyboard.preview.value);
-                    if (this.props.debug) {
-                        console.log('The content "' + el.value + '" was changed');
-                    }
-                }.bind(this);
-            }
+  return VirtualKeyboard;
+}(_react.Component);
 
-            // Add jQuery Keyboard to DOM Element
-            // jQuery(ReactDOM.findDOMNode(this.refs.keyboard)).keyboard(this.props.options);
-            this.addKeyBoardToDOM();
-
-            // Update while typing if usePreview is false
-            if (this.props.options.usePreview === false) {
-                (0, _jquery2.default)(_reactDom2.default.findDOMNode(this.refs.keyboard)).on('keyboardChange', function (event, keyboard, el) {
-                    this.handleChange(null, keyboard.preview.value);
-                }.bind(this));
-            }
-        }
-    }, {
-        key: 'addKeyBoardToDOM',
-        value: function addKeyBoardToDOM() {
-            (0, _jquery2.default)(_reactDom2.default.findDOMNode(this.refs.keyboard)).keyboard(this.props.options);
-        }
-    }, {
-        key: 'clear',
-        value: function clear() {
-            this.setState({ value: '' });
-        }
-    }, {
-        key: 'select',
-        value: function select() {
-            this.refs.keyboard.select();
-        }
-    }, {
-        key: 'blur',
-        value: function blur() {
-            var keyboard = (0, _jquery2.default)(_reactDom2.default.findDOMNode(this.refs.keyboard));
-            if (keyboard && typeof keyboard.getkeyboard == 'function' && typeof keyboard.getkeyboard().close == 'function') keyboard.getkeyboard().close();
-            this.refs.keyboard.blur();
-        }
-    }, {
-        key: 'checkValidity',
-        value: function checkValidity() {
-            return this.refs.keyboard.checkValidity();
-        }
-    }, {
-        key: 'handleChange',
-        value: function handleChange(event, input) {
-            if (!input && event && event.target && typeof event.target.value != 'undefined') input = event.target.value;
-            if (this.props.debug) {
-                console.log("Change", input);
-            }
-            this.setState({ value: input });
-            this.props.onChange(input);
-        }
-    }, {
-        key: 'componentWillUnmount',
-        value: function componentWillUnmount() {
-            (0, _jquery2.default)(_reactDom2.default.findDOMNode(this.refs.keyboard)).remove();
-        }
-    }, {
-        key: 'render',
-        value: function render() {
-            var _props = this.props,
-                options = _props.options,
-                value = _props.value,
-                validation = _props.validation,
-                onChange = _props.onChange,
-                other = _objectWithoutProperties(_props, ['options', 'value', 'validation', 'onChange']);
-
-            if (this.props.options.type == 'textarea') {
-                var element = _react2.default.createElement('textarea', _extends({ ref: 'keyboard', value: this.state.value, onChange: this.handleChange }, other));
-            } else {
-                var element = _react2.default.createElement('input', _extends({ ref: 'keyboard', value: this.state.value, onChange: this.handleChange }, other));
-            }
-            return _react2.default.createElement(
-                'div',
-                { className: this.state.className },
-                ' ',
-                element,
-                ' '
-            );
-        }
-    }]);
-
-    return VirtualKeyboard;
-}(_react2.default.Component);
+VirtualKeyboard.propTypes = {
+  value: _propTypes2.default.string,
+  options: _propTypes2.default.object,
+  onAccepted: _propTypes2.default.func,
+  onChange: _propTypes2.default.func,
+  debug: _propTypes2.default.bool,
+  validation: _propTypes2.default.func
+};
 
 exports.default = VirtualKeyboard;
